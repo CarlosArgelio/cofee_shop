b070165913f8986c5484f2ba1a3b992d8904d797
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-01 14:21:11 -0400

    relation 1:N

--- coffee_shop/app/models.py
+++ coffee_shop/app/models.py
@@ -14,10 +14,18 @@ class Publisher(models.Model):
     def __str__(self) -> str:
         return self.name
 
+class Author(models.Model):
+    name = models.TextField(max_length=200)
+    birt_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.name
+
 class Book(models.Model):
     title = models.TextField(max_length=200)
     publication_date = models.DateField()
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name="authors")
 
     def __str__(self) -> str:
         return self.title
