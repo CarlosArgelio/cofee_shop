2af14b4850fe60d4c23b3c6d82f30fc51d16ab50
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-02 19:45:14 -0400

    feat: create model and make migrations

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
+# Models
 from django.db import models
+from django.db.models import TextField, DecimalField, BooleanField, ImageField
 
-# Create your models here.
+
+class Product(models.Model):
+    name = TextField(max_length=200, verbose_name="nombre")
+    description = TextField(max_length=300, verbose_name="descripcion")
+    price = DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
+    available = BooleanField(default=True, verbose_name="disponible")
+    photo = ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
+
+    def __str__(self) -> str:
+        return self.name
