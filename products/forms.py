d210d03589be934836fceb6af89ff0a03aec4d62
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-02 20:33:40 -0400

    feat: add form

---
+++ products/forms.py
@@ -0,0 +1,22 @@
+# Forms
+from django import forms
+from django.forms import CharField, DecimalField, BooleanField, ImageField
+
+from products.models import Product
+
+
+class ProductForm(forms.Form):
+    name = CharField(max_length=200, label="Nombre")
+    description = CharField(max_length=200, label="Descripcion")
+    price = DecimalField(max_digits=10, decimal_places=2, label="Precio")
+    available = BooleanField(initial=True, label="Disponible", required=False)
+    photo = ImageField(label="Foto", required=False)
+
+    def save(self):
+        Product.objects.create(
+            name=self.cleaned_data['name'],
+            description=self.cleaned_data['description'],
+            price=self.cleaned_data['price'],
+            available=self.cleaned_data['available'],
+            photo=self.cleaned_data['photo'],
+        )
