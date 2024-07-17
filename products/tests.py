8ec161f68238d9c137ad1834bc972ecc43f301b0
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-17 17:06:47 -0400

    feat: add testing

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ProductListViewTest(TestCase):
+
+    def test_should_return_200_when_load_page(self):
+        url = reverse('list_product')
+
+        response = self.client.get(url)
+        status_code = response.status_code
+
+        self.assertEqual(status_code, 200)
